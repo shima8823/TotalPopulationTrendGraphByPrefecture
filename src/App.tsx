eb078533687fdd13a945c66d4c6adb42fdf5a511
shima8823 <taiyaki1275@gmail.com>
2023-07-21 12:55:33 +0900

    feat: close #3 APIから人口構成を取得する

--- src/App.tsx
+++ src/App.tsx
@@ -13,6 +13,9 @@ export default function MyApp() {
   const [arrayOfPrefecture, setListOfPrefecture] = useState<
     PrefecturesData[] | null
   >(null)
+  const [checkedPrefectures, setCheckedPrefectures] = useState<{
+    [key: string]: boolean
+  }>({})
 
   useEffect(() => {
     axios
@@ -21,20 +24,47 @@ export default function MyApp() {
       })
       .then((res) => {
         setListOfPrefecture(res.data.result)
+        const initialChecks = res.data.result.reduce(
+          (acc: { [key: string]: boolean }, curr: PrefecturesData) => ({
+            ...acc,
+            [curr.prefName]: false
+          }),
+          {}
+        )
+        setCheckedPrefectures(initialChecks)
       })
       .catch(() => {})
   }, [])
 
-  const onChange = () => {}
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setCheckedPrefectures({
+      ...checkedPrefectures,
+      [e.target.value]: e.target.checked
+    })
+    if (e.target.checked) {
+      axios
+        .get(
+          `${RESAS_BASEURL}api/v1/population/composition/perYear?cityCode=-&prefCode=${e.target.value}`,
+          {
+            headers: { 'X-API-KEY': process.env.REACT_APP_RESAS_KEY }
+          }
+        )
+        .then((res) => {
+          // 保存
+          // eslint-disable-next-line
+          console.log(res)
+        })
+    }
+  }
   return (
     <div>
       {arrayOfPrefecture
         ? arrayOfPrefecture.map((prefecture) => (
             <div key={prefecture.prefCode}>
               <input
-                id={prefecture.prefName}
+                id={prefecture.prefCode.toString()}
                 type="checkbox"
-                value={prefecture.prefName}
+                value={prefecture.prefCode}
                 onChange={onChange}
               />
               <label htmlFor={prefecture.prefName}>{prefecture.prefName}</label>
